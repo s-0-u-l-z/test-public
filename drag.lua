9404222f1618730f7697f80530034f2788120c58
s-0-u-l-z <ttvpro2612@gmail.com>
2025-08-17 08:29:14 -0400

    Create drag.lua

---
+++ drag.lua
@@ -0,0 +1,17 @@
+-- Improved dragging functionality
+local dragging
+local dragInput
+local dragStart
+local startPos
+
+titleBar.InputBegan:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 then
+        dragging = true
+        dragStart = input.Position
+        startPos = mainFrame.Position
+        
+        input.Changed:Connect(function()
+            if input.UserInputState == Enum.UserInputState.End then dragging = false end
+        end)
+    end
+end)
